51932c1ac513324f56e82a9916b8cd39fef175b3
Matthew Ferderber <matthewferderber@gmail.com>
2018-01-24 02:44:04 -0500

    add javafx MainView

--- src/main/java/MyMoneyDriver.java
+++ src/main/java/MyMoneyDriver.java
@@ -1,7 +1,23 @@
 package main.java;
 
-public class MyMoneyDriver {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class MyMoneyDriver extends Application {
 
 	public static void main(String[] args) {
+		launch(args);
+	}
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		Parent root = FXMLLoader.load(getClass().getResource("/main/resources/view/MainView.fxml"));
+		Scene scene = new Scene(root, 800, 600);
+		primaryStage.setTitle("MyMoney Application");
+		primaryStage.setScene(scene);
+		primaryStage.show();
 	}
 }
