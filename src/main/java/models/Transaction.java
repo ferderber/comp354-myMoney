0faeb2cb0f7e59bd3d44b435760efabf5107bc7b
Matthew Ferderber <matthewferderber@gmail.com>
2018-01-25 03:17:46 -0500

    added mut/acc to Transaction

--- src/main/java/models/Transaction.java
+++ src/main/java/models/Transaction.java
@@ -33,4 +33,36 @@ public class Transaction {
 		this.date = date;
 	}
 
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public Date getDate() {
+		return date;
+	}
+
+	public void setDate(Date date) {
+		this.date = date;
+	}
+
 }
