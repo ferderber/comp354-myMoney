29a99d9cf7094999b49f79f5c6bb1a6fa3e542c9
Matthew Ferderber <matthewferderber@gmail.com>
2018-01-24 23:52:20 -0500

    fix DatabaseField annotations

--- src/main/java/models/Transaction.java
+++ src/main/java/models/Transaction.java
@@ -7,14 +7,23 @@ import com.j256.ormlite.table.DatabaseTable;
 
 @DatabaseTable
 public class Transaction {
-	@DatabaseField(id = true)
+	@DatabaseField(generatedId = true)
 	private int id;
+	@DatabaseField
 	private String name;
+	@DatabaseField
 	private String description;
+	@DatabaseField
 	private Date date;
 
 	protected Transaction() {
 
+	}
+
+	public Transaction(String name, String description, Date date) {
+		this.name = name;
+		this.description = description;
+		this.date = date;
 	}
 
 	public Transaction(int id, String name, String description, Date date) {
