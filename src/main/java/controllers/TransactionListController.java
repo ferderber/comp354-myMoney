db4e746d90d0c5a25cad16438c5fbe6e82cc1c7a
Matthew Ferderber <matthewferderber@gmail.com>
2018-01-24 20:30:19 -0500

    add sample layout to transactionListController

--- src/main/java/controllers/TransactionListController.java
+++ src/main/java/controllers/TransactionListController.java
@@ -1,5 +1,31 @@
 package main.java.controllers;
 
+import java.util.List;
+
+import javafx.fxml.FXML;
+import main.java.dao.TransactionDao;
+import main.java.models.Transaction;
+
 public class TransactionListController {
 
+	public TransactionListController() {
+		System.out.println("Creating Transaction List Controller");
+	}
+
+	/**
+	 * Dispatches editTransaction event to MainController
+	 * 
+	 * @return
+	 */
+	@FXML
+	private int editTransaction() {
+		// TODO: Implement event dispatching.
+		return 0;
+	}
+
+	@FXML
+	private List<Transaction> getTransactions() {
+		TransactionDao transactionDao = new TransactionDao();
+		return transactionDao.getAllTransactions();
+	}
 }
