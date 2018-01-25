43fc438307e3c2674c8806dca8b9915a76fbb399
Matthew Ferderber <matthewferderber@gmail.com>
2018-01-25 03:16:21 -0500

    added insert to populate test db

--- src/main/java/MyMoneyDriver.java
+++ src/main/java/MyMoneyDriver.java
@@ -1,10 +1,14 @@
 package main.java;
 
+import java.util.Date;
+
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import main.java.dao.TransactionDao;
+import main.java.models.Transaction;
 
 public class MyMoneyDriver extends Application {
 
@@ -14,6 +18,8 @@ public class MyMoneyDriver extends Application {
 
 	@Override
 	public void start(Stage primaryStage) throws Exception {
+		TransactionDao dao = new TransactionDao();
+		dao.insert(new Transaction("Sample Transaction", "Sample Description", new Date()));
 		Parent root = FXMLLoader.load(getClass().getResource("/main/resources/view/MainView.fxml"));
 		Scene scene = new Scene(root, 800, 600);
 		primaryStage.setTitle("MyMoney Application");
