3d35c393731e6b1a0d6200d279ebaa74fdbcef77
Matthew Ferderber <matthewferderber@gmail.com>
2018-02-11 18:38:10 -0500

    use GridPane for formatting

--- src/main/java/controllers/AccountSingleController.java
+++ src/main/java/controllers/AccountSingleController.java
@@ -13,6 +13,7 @@ import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.FlowPane;
+import javafx.scene.layout.GridPane;
 import javafx.scene.layout.VBox;
 import main.java.dao.AccountDao;
 import main.java.models.Account;
@@ -39,30 +40,27 @@ public class AccountSingleController implements Initializable {
 	private TextField typeField;
 	@FXML
 	private TextField balanceField;
-	
+
 	@FXML
-	private FlowPane singleAccountContainer;
-	@FXML
-	private VBox accountAddBox;
+	private GridPane singleAccountContainer;
 	@FXML
 	private ObjectProperty<Account> accountProperty = new SimpleObjectProperty<>();
-	
+
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
 	}
-	
-	
+
 	@FXML
 	public void saveAccount() {
 		Account a = accountProperty.get();
-		if(a == null) {
-		 a = new Account();
-		 a.setCreate(new Date());
-		 a.setEdit(null);
-		 a.setArchived(null);
+		if (a == null) {
+			a = new Account();
+			a.setCreate(new Date());
+			a.setEdit(null);
+			a.setArchived(null);
 		}
 		String name = nameField.getText();
-		
+
 		long number = 0L;
 		try {
 			number = Long.parseLong(numberField.getText());
@@ -72,9 +70,8 @@ public class AccountSingleController implements Initializable {
 		AccountType type = null;
 		try {
 			type = AccountType.valueOf(typeField.getText());
-		}
-		catch(Exception ex) {
-			
+		} catch (Exception ex) {
+
 		}
 		double balance = 0.0;
 		try {
@@ -82,32 +79,34 @@ public class AccountSingleController implements Initializable {
 		} catch (Exception ex) {
 			// TODO: Display validation error
 		}
-		
+
 		a.setName(name);
 		a.setBalance(balance);
 		a.setNumber(number);
 		a.setType(type);
 		try {
-		if (accountProperty.get() == null) {
-			dao.insert(a);
-		} else {
-			dao.updateAccount(a);
+			if (accountProperty.get() == null) {
+				dao.insert(a);
+			} else {
+				dao.updateAccount(a);
+			}
+		} catch (Exception ex) {
+			// TODO
 		}
-		}
-		catch(Exception ex) {
-			//TODO
-		}
-		//AccountController b = new AccountController();b.switchAccountView();
-		//FXMLLoader loader = new FXMLLoader(getClass().getResource("AccountView.fxml"));
-		//AccountController controller = loader.getController();
-		//controller.setaccountView();
-		//AccountController.switchAccountView();
+		// AccountController b = new AccountController();b.switchAccountView();
+		// FXMLLoader loader = new
+		// FXMLLoader(getClass().getResource("AccountView.fxml"));
+		// AccountController controller = loader.getController();
+		// controller.setaccountView();
+		// AccountController.switchAccountView();
 	}
-	@FXML 
+
+	@FXML
 	public void backToAccountList() {
-		//AccountController.switchAccountView();
+		// AccountController.switchAccountView();
 
 	}
+
 	public ObjectProperty<Account> accountProperty() {
 		return accountProperty;
 	}
@@ -115,6 +114,7 @@ public class AccountSingleController implements Initializable {
 	public Account getAccount() {
 		return accountProperty.get();
 	}
+
 	/**
 	 * Sets the transactionClickHandler for each TransactionView
 	 * 
