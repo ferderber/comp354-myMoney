733a595a70e1afa45ce477d12c2ea99073dcb76e
KNG-Scherezade <kai.nicoll.griffith@gmail.com>
2018-04-02 02:39:32 -0400

    budget-test initial commit

---
+++ src/test/account/budget/BudgetTest.java
@@ -0,0 +1,23 @@
+package test.account.budget;
+
+public class BudgetTest {
+
+	public void testProgressBar() {
+		//check progress bar displays proper values
+	}
+	
+	public void testAccountClicked() {
+		//check interface created, no data
+		//check interface created, has data
+	}
+
+	
+	public void testBudgetInput() {
+		//test propper
+		//test empty
+	}
+	
+
+	
+	
+}
