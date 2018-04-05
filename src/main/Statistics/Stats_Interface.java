f4ce98cdf9d14cad45edcf4b9f0439ecd60be919
Matthew Ferderber <matthewferderber@gmail.com>
2018-04-05 12:20:34 -0400

    Refactor Statistics & fix empty transaction bugs

--- src/main/Statistics/Stats_Interface.java
+++ src/main/Statistics/Stats_Interface.java
@@ -1,490 +1,443 @@
 package main.Statistics;
 
-
 import java.util.ArrayList;
 import java.util.List;
 
+import main.java.dao.TransactionDao;
 import main.java.models.Transaction;
 
-public class Stats_Interface 
-{
-	
-	public List<String> ExtremeMonths()
-	{
+public class Stats_Interface {
+
+	public List<String> ExtremeMonths(List<Transaction> list) {
 		Statistics stat = new Statistics();
-		
+
 		List<String> array = new ArrayList<String>();
-		
+
 		String temp = "";
-		
-		List<Transaction> list = stat.getAllTransactions();
-		
-		double TotalAvg = stat.getTotalAverage();
-		double UpperTotalAvg = TotalAvg *1.05;
-		double LowerTotalAvg = TotalAvg *0.95;
-		
-		double TotalMed = stat.getTotalMedian();
-		double UpperTotalMed = TotalMed *1.05;
-		double LowerTotalMed = TotalMed *0.95;
-		
-		double TotalIN = stat.getAverageIn();
-		double UpperTotalIN = TotalIN *1.05;
-		double LowerTotalIN = TotalIN *0.95;
-		
-		double TotalOUT = stat.getAverageOut();
-		double UpperTotalOUT = TotalOUT *1.05;
-		double LowerTotalOUT = TotalOUT *0.95;
-		
-		Transaction transMaxIN = stat.getMaxIn();
-		Transaction transMaxOUT = stat.getMaxOut();
-		
-		int MaxInYear = transMaxIN.getDate().getYear()+1900;
+
+		double TotalAvg = stat.getTotalAverage(list);
+		double UpperTotalAvg = TotalAvg * 1.05;
+		double LowerTotalAvg = TotalAvg * 0.95;
+
+		double TotalMed = stat.getTotalMedian(list);
+		double UpperTotalMed = TotalMed * 1.05;
+		double LowerTotalMed = TotalMed * 0.95;
+
+		double TotalIN = stat.getAverageIn(list);
+		double UpperTotalIN = TotalIN * 1.05;
+		double LowerTotalIN = TotalIN * 0.95;
+
+		double TotalOUT = stat.getAverageOut(list);
+		double UpperTotalOUT = TotalOUT * 1.05;
+		double LowerTotalOUT = TotalOUT * 0.95;
+
+		Transaction transMaxIN = stat.getMaxIn(list);
+		Transaction transMaxOUT = stat.getMaxOut(list);
+
+		int MaxInYear = transMaxIN.getDate().getYear() + 1900;
 		int MaxInMonth = transMaxIN.getDate().getMonth();
-		
+
 		temp = "BIGGEST INFLOW/OUTFLOW\n";
 		array.add(temp);
 		temp = "";
-		
-		temp = "Biggest Inflow is in the month of " + toMonth(MaxInMonth) + " in " + MaxInYear+ "\n" + " Amount: $" + transMaxIN.getAmount() +"\n";
+
+		temp = "Biggest Inflow is in the month of " + toMonth(MaxInMonth) + " in " + MaxInYear + "\n" + " Amount: $"
+				+ transMaxIN.getAmount() + "\n";
 		array.add(temp);
 		temp = "";
-		
-		int MaxOutYear = transMaxOUT.getDate().getYear()+1900;
+
+		int MaxOutYear = transMaxOUT.getDate().getYear() + 1900;
 		int MaxOutMonth = transMaxOUT.getDate().getMonth();
-		
-		temp = "Biggest Outflow is in the month of " + toMonth(MaxOutMonth) + " in " + MaxOutYear+ "\n"+ " Amount: $" + transMaxOUT.getAmount() +"\n";
+
+		temp = "Biggest Outflow is in the month of " + toMonth(MaxOutMonth) + " in " + MaxOutYear + "\n" + " Amount: $"
+				+ transMaxOUT.getAmount() + "\n";
 		array.add(temp);
 		temp = "";
-		
+
 		int monthsDone = 0;
-		int months = stat.getTotalMonths();
-		ArrayList<Integer> year = stat.getYearList();
-		
+		int months = stat.getTotalMonths(list);
+		ArrayList<Integer> year = stat.getYearList(list);
+
 		double MonthAvg = 0;
 		double MonthMed = 0;
 		double MonthIN = 0;
 		double MonthOUT = 0;
-		
-		
+
 		int Cyear = year.get(0);
 		int Cmonth = 0;
-		
+
 		temp = "EXTREME EVENTS BY MONTH\n";
 		array.add(temp);
 		temp = "";
-		
-		while(months != 0)
-		{
-			if(monthsDone >= 12)
-			{
+
+		while (months != 0) {
+			if (monthsDone >= 12) {
 				monthsDone = 0;
 				Cmonth = 0;
-				
-				for(int i=0; i<year.size(); i++)
-				{
-					if(year.get(i)> Cyear)
-					{
+
+				for (int i = 0; i < year.size(); i++) {
+					if (year.get(i) > Cyear) {
 						Cyear = year.get(i);
-					}
-					else
-					{
+					} else {
 						Cyear = -1;
 					}
 				}
-				
+
 			}
-			
-			
-			if(Cyear != -1)
-			{
-				temp = "\nMONTH: " + toMonth(Cmonth) + " YEAR: " + Cyear +"\n";
+
+			if (Cyear != -1) {
+				temp = "\nMONTH: " + toMonth(Cmonth) + " YEAR: " + Cyear + "\n";
 				array.add(temp);
 				temp = "";
 			}
-			
-			MonthAvg = stat.getAverageByYearAndMonth(Cmonth, Cyear);
-			
-			if(MonthAvg != 0)
-			{
-				MonthMed = stat.getMedianByYearAndMonth(Cmonth, Cyear);
-				MonthIN = stat.getAverageInByYearAndMonth(Cmonth, Cyear);
-				MonthOUT = stat.getAverageOutByYearAndMonth(Cmonth, Cyear);
-				
-				if(MonthAvg > UpperTotalAvg)
-				{
+
+			MonthAvg = stat.getAverageByYearAndMonth(list, Cmonth, Cyear);
+
+			if (MonthAvg != 0) {
+				MonthMed = stat.getMedianByYearAndMonth(list, Cmonth, Cyear);
+				MonthIN = stat.getAverageInByYearAndMonth(list, Cmonth, Cyear);
+				MonthOUT = stat.getAverageOutByYearAndMonth(list, Cmonth, Cyear);
+
+				if (MonthAvg > UpperTotalAvg) {
 					temp = "Above Avg Income:" + "\n" + " Avg: $" + TotalAvg + " Actual: $" + MonthAvg + "\n";
 					array.add(temp);
 					temp = "";
 				}
-				
-				if(MonthAvg < LowerTotalAvg)
-				{
-					temp = "Below Avg Income:" + "\n"  + " Avg: $" + TotalAvg + " Actual: $" + MonthAvg+ "\n";
+
+				if (MonthAvg < LowerTotalAvg) {
+					temp = "Below Avg Income:" + "\n" + " Avg: $" + TotalAvg + " Actual: $" + MonthAvg + "\n";
 					array.add(temp);
 					temp = "";
 				}
-				
-				if(MonthMed < LowerTotalMed)
-				{
-					temp = "Below Median Income:" + "\n"  + " Med: $" + TotalMed + " Actual: $" + MonthMed+ "\n";
+
+				if (MonthMed < LowerTotalMed) {
+					temp = "Below Median Income:" + "\n" + " Med: $" + TotalMed + " Actual: $" + MonthMed + "\n";
 					array.add(temp);
 					temp = "";
 				}
-				if(MonthMed > UpperTotalMed)
-				{
-					temp = "Above Median Income:" + "\n"  + " Med: $" + TotalMed + " Actual: $" + MonthMed+ "\n";
+				if (MonthMed > UpperTotalMed) {
+					temp = "Above Median Income:" + "\n" + " Med: $" + TotalMed + " Actual: $" + MonthMed + "\n";
 					array.add(temp);
 					temp = "";
 				}
-				
-				if(MonthIN < LowerTotalIN)
-				{
-					temp = "Inflow of Income lower than average:" + "\n"  + " IN Avg: $" + TotalIN + " Actual: $" + MonthIN+ "\n";
+
+				if (MonthIN < LowerTotalIN) {
+					temp = "Inflow of Income lower than average:" + "\n" + " IN Avg: $" + TotalIN + " Actual: $"
+							+ MonthIN + "\n";
 					array.add(temp);
 					temp = "";
 				}
-				if(MonthIN > UpperTotalIN)
-				{
-					temp = "Inflow of Income higher than average:" + "\n"  + " IN Avg: $" + TotalIN + " Actual: $" + MonthIN+ "\n";
+				if (MonthIN > UpperTotalIN) {
+					temp = "Inflow of Income higher than average:" + "\n" + " IN Avg: $" + TotalIN + " Actual: $"
+							+ MonthIN + "\n";
 					array.add(temp);
 					temp = "";
 				}
-				
-				if(MonthOUT < LowerTotalOUT)
-				{
-					temp = "Outflow of Income lower than average:" + "\n"  + " OUT Avg: $" + TotalOUT + " Actual: $" + MonthOUT+ "\n";
+
+				if (MonthOUT < LowerTotalOUT) {
+					temp = "Outflow of Income lower than average:" + "\n" + " OUT Avg: $" + TotalOUT + " Actual: $"
+							+ MonthOUT + "\n";
 					array.add(temp);
 					temp = "";
 				}
-				if(MonthOUT > UpperTotalOUT)
-				{
-					temp = "Outflow of Income higher than average:" + "\n"  + " OUT Avg: $" + TotalOUT + " Actual: $" + MonthOUT+ "\n";
+				if (MonthOUT > UpperTotalOUT) {
+					temp = "Outflow of Income higher than average:" + "\n" + " OUT Avg: $" + TotalOUT + " Actual: $"
+							+ MonthOUT + "\n";
 					array.add(temp);
 					temp = "";
 				}
 			}
-			
+
 			Cmonth += 1;
 			monthsDone += 1;
 			months -= 1;
-			
-			
-			
+
 		}
-		
-		
+
 		return array;
 	}
-	
-	public List<Transaction> select(String select)
-	{
+
+	public List<Transaction> select(List<Transaction> list, String select) {
 		String Temp = "";
 		String Temp2 = "";
-		
+
 		int trans = 0;
 		int trans2 = 0;
-		
+
 		Statistics stat = new Statistics();
-		
-		
-		if(select.charAt(0) == 'a' || select.charAt(0) == 'A')
-		{
-			return stat.getAllTransactions();
+
+		if (select.charAt(0) == 'a' || select.charAt(0) == 'A') {
+			return list;
 		}
-		
-		if(select.charAt(0) == '2' && select.charAt(1) == '0' && select.charAt(2) == '1')
-		{
+
+		if (select.charAt(0) == '2' && select.charAt(1) == '0' && select.charAt(2) == '1') {
 			trans = Integer.parseInt(select);
-			return stat.getTransactionByYear(trans);
+			return stat.getTransactionByYear(list, trans);
 		}
-		
-		if((int)select.charAt(0) > 47 && (int)select.charAt(0) < 58)
-		{
+
+		if ((int) select.charAt(0) > 47 && (int) select.charAt(0) < 58) {
 			trans = Integer.parseInt(select);
-			return stat.getMonthsTransactions(trans);
+			return stat.getMonthsTransactions(list, trans);
 		}
-		
-		for(int i=0; i<select.length(); i++)
-		{
-			if((int)select.charAt(i) == 32)
-			{
+
+		for (int i = 0; i < select.length(); i++) {
+			if ((int) select.charAt(i) == 32) {
 				Temp = select.substring(0, i);
-				Temp2 = select.substring(i+1, select.length());
+				Temp2 = select.substring(i + 1, select.length());
 				trans = Integer.parseInt(Temp2);
 				trans2 = toMonth(Temp);
-				
-				return stat.getTransactionByMonth(trans2, trans);
+
+				return stat.getTransactionByMonth(list, trans2, trans);
 			}
 		}
-		
+
 		return null;
 	}
-	
-	public List<String> displayStats(String select, String Type)
-	{
+
+	public List<String> displayStats(List<Transaction> allTransactions, String select, String Type) {
 		Statistics stat = new Statistics();
-		
-		List<Transaction> list = select(select);
-		
-		if(!(Type == null))
-		{
+
+		List<Transaction> list = select(allTransactions, select);
+
+		if (!(Type == null)) {
 			list = stat.RetrunByType(Type, list);
 		}
-		
+
 		List<String> array = new ArrayList<String>();
-		
+
 		String temp = "";
-		
-		if(list == null)
-		{
+
+		if (list == null) {
 			array.add("Empty");
 			return array;
 		}
-		if(list.isEmpty())
-		{
+		if (list.isEmpty()) {
 			array.add("Empty");
 			return array;
 		}
-		
+
 		temp = "THE STATISTICS FOR YOUR SELECTION: " + select + "\n";
 		array.add(temp);
 		temp = "";
-		
+
 		double Avg = stat.getAverage(list);
-		
+
 		temp = "The Average is: " + Avg + "\n";
 		array.add(temp);
 		temp = "";
-		
+
 		double Med = stat.getMedian(list);
-		
+
 		temp = "The Median is: " + Med + "\n";
 		array.add(temp);
 		temp = "";
-		
+
 		double InAvg = stat.getAverageIn(list);
-		
+
 		temp = "The Average Inflow is: " + InAvg + "\n";
 		array.add(temp);
 		temp = "";
-		
+
 		double OutAvg = stat.getAverageOut(list);
-		
+
 		temp = "The Average Outflow is: " + OutAvg + "\n";
 		array.add(temp);
 		temp = "";
-		
+
 		double InMed = stat.getMedianIn(list);
-		
+
 		temp = "The Median Inflow is: " + InMed + "\n";
 		array.add(temp);
 		temp = "";
-		
+
 		double OutMed = stat.getMedianOut(list);
-		
+
 		temp = "The Median Outflow is: " + OutMed + "\n";
 		array.add(temp);
 		temp = "";
-		
+
 		return array;
 	}
-	
-	public List<String> displayRecurring(String select, int times, String Type)
-	{
+
+	public List<String> displayRecurring(List<Transaction> allTransactions, String select, int times, String Type) {
 		Statistics stat = new Statistics();
-		
-		List<Transaction> list = select(select);
-		
+
+		List<Transaction> list = select(allTransactions, select);
+
 		List<String> array = new ArrayList<String>();
-		
-		if(!(Type == null))
-		{
+
+		if (!(Type == null)) {
 			list = stat.RetrunByType(Type, list);
 		}
-		
+
 		String temp = "";
-		
+
 		list = stat.getRecurring(list, times);
-		
-		if(list.isEmpty())
-		{
+
+		if (list.isEmpty()) {
 			array.add("Empty");
 			return array;
 		}
-		
-		
+
 		temp = "RECURRING TRANSACTIONS\n";
 		array.add(temp);
 		temp = "";
-		
-		for(int i=0; i<list.size(); i++)
-		{
-			temp = "Transaction Name: " + list.get(i).getName() + "\nTransaction Amount: $" + list.get(i).getAmount() + "\n";
+
+		for (int i = 0; i < list.size(); i++) {
+			temp = "Transaction Name: " + list.get(i).getName() + "\nTransaction Amount: $" + list.get(i).getAmount()
+					+ "\n";
 			array.add(temp);
 			temp = "";
-			
+
 		}
-		
+
 		return array;
 	}
-	
-	public BarChart displayYearStats()
-	{
-		
+
+	public BarChart displayYearStats(List<Transaction> list) {
+
 		Statistics stat = new Statistics();
-		
-		List<Transaction> list = stat.getAllTransactions();
+
 		List<String> Names = new ArrayList<String>();
-		List<Integer> Years = stat.getYearList();
+		List<Integer> Years = stat.getYearList(list);
 		List<Double> MAvg = new ArrayList<Double>();
 		List<Double> MMed = new ArrayList<Double>();
 		List<Double> MI = new ArrayList<Double>();
 		List<Double> MO = new ArrayList<Double>();
-		
+
 		int Cyear = 0;
 		int Cmonth = 0;
-		
+
 		String Time = "";
-		
-		if(list.size()>0)
-		{
-			Cyear = list.get(list.size()-1).getDate().getYear() + 1900;
-			Cmonth = list.get(list.size()-1).getDate().getMonth();
+
+		if (list.size() > 0) {
+			Cyear = list.get(list.size() - 1).getDate().getYear() + 1900;
+			Cmonth = list.get(list.size() - 1).getDate().getMonth();
 		}
-		
+
 		Cmonth = Cmonth - 12;
-		
-		if(Cmonth < 0)
-		{
+
+		if (Cmonth < 0) {
 			Cyear -= 1;
 			Cmonth += 12;
-			
-			if(Cmonth == 12)
-			{
+
+			if (Cmonth == 12) {
 				Cmonth = 0;
 			}
 		}
-		
-		
+
 		double MonthAvg = 0;
 		double MonthMed = 0;
 		double MonthIN = 0;
 		double MonthOUT = 0;
-		
+
 		Time = Cyear + " ";
-		
-		for(int i=0; i<13; i++)
-		{
-			MonthAvg = stat.getAverageByYearAndMonth(Cmonth, Cyear);
-			MonthMed = stat.getMedianByYearAndMonth(Cmonth, Cyear);
-			MonthIN = stat.getAverageInByYearAndMonth(Cmonth, Cyear);
-			MonthOUT = stat.getAverageOutByYearAndMonth(Cmonth, Cyear);
-			
+
+		for (int i = 0; i < 13; i++) {
+			MonthAvg = stat.getAverageByYearAndMonth(list, Cmonth, Cyear);
+			MonthMed = stat.getMedianByYearAndMonth(list, Cmonth, Cyear);
+			MonthIN = stat.getAverageInByYearAndMonth(list, Cmonth, Cyear);
+			MonthOUT = stat.getAverageOutByYearAndMonth(list, Cmonth, Cyear);
+
 			MAvg.add(MonthAvg);
 			MMed.add(MonthMed);
 			MI.add(MonthIN);
 			MO.add(MonthOUT);
 			Names.add(toMonth(Cmonth));
-			
+
 			Cmonth += 1;
-			
-			if(Cmonth > 11)
-			{
+
+			if (Cmonth > 11) {
 				Cmonth = 0;
 				Cyear += 1;
 				Time += Cyear;
 			}
 		}
-		
+
 		BarChart bc = new BarChart(MAvg, MMed, MI, MO, Names, Time);
-		
+
 		return bc;
 	}
-	
-	public String toMonth(int month)
-	{
-		switch(month)
-		{
-			case 0:
-				return "January";
-			case 1:
-				return "Febuary";
-			case 2:
-				return "March";
-			case 3:
-				return "April";
-			case 4:
-				return "May";
-			case 5:
-				return "June";
-			case 6:
-				return "July";
-			case 7:
-				return "August";
-			case 8:
-				return "September";
-			case 9:
-				return "October";
-			case 10:
-				return "November";
-			case 11:
-				return "December";
-				
+
+	public String toMonth(int month) {
+		switch (month) {
+		case 0:
+			return "January";
+		case 1:
+			return "Febuary";
+		case 2:
+			return "March";
+		case 3:
+			return "April";
+		case 4:
+			return "May";
+		case 5:
+			return "June";
+		case 6:
+			return "July";
+		case 7:
+			return "August";
+		case 8:
+			return "September";
+		case 9:
+			return "October";
+		case 10:
+			return "November";
+		case 11:
+			return "December";
+
 		}
-		
+
 		return null;
 	}
-	
-	public int toMonth(String month)
-	{
-		switch(month)
-		{
-			case "January":
-				return 0;
-			case "Febuary":
-				return 1;
-			case "March":
-				return 2;
-			case "April":
-				return 3;
-			case "May":
-				return 4;
-			case "June":
-				return 5;
-			case "July":
-				return 6;
-			case "August":
-				return 7;
-			case "September":
-				return 8;
-			case "October":
-				return 9;
-			case "November":
-				return 10;
-			case "December":
-				return 11;
-				
+
+	public int toMonth(String month) {
+		switch (month) {
+		case "January":
+			return 0;
+		case "Febuary":
+			return 1;
+		case "March":
+			return 2;
+		case "April":
+			return 3;
+		case "May":
+			return 4;
+		case "June":
+			return 5;
+		case "July":
+			return 6;
+		case "August":
+			return 7;
+		case "September":
+			return 8;
+		case "October":
+			return 9;
+		case "November":
+			return 10;
+		case "December":
+			return 11;
+
 		}
-		
+
 		return 12;
 	}
-	
-	
-	public static void main(String args[])
-	{
+
+	public static void main(String args[]) {
+		TransactionDao dao = new TransactionDao();
+		List<Transaction> allTransactions = dao.getAllTransactions();
 		Stats_Interface inter = new Stats_Interface();
-		
+
 		List<String> list = new ArrayList<String>();
-		
-		BarChart bc  = inter.displayYearStats();
-		
-		list = inter.displayStats("March", null);
-		
-		for(int i=0; i<bc.size; i++)
-		{
+
+		BarChart bc = inter.displayYearStats(allTransactions);
+
+		list = inter.displayStats(allTransactions, "March", null);
+
+		for (int i = 0; i < bc.size; i++) {
 			System.out.println(bc.MonthAvg.get(i));
 		}
-		
+
 		System.out.println(bc.Year);
 	}
 }
--- src/main/java/controllers/StatisticsController.java
+++ src/main/java/controllers/StatisticsController.java
@@ -17,6 +17,8 @@ import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.Text;
+import main.java.dao.TransactionDao;
+import main.java.models.Transaction;
 import main.java.views.TransactionView;
 
 import javafx.application.Application;
@@ -27,151 +29,132 @@ import javafx.scene.chart.NumberAxis;
 import javafx.scene.chart.XYChart;
 import javafx.stage.Stage;
 
-
-public class StatisticsController implements Initializable
-{
+public class StatisticsController implements Initializable {
 	@FXML
 	private VBox container;
 
 	@FXML
 	private List<String> transactionViews;
-	
+
 	@FXML
 	private Text transactionName;
-	
+
 	@FXML
 	private Button updateStatisticsButton;
-	
+
 	@FXML
 	private TextField transactionType;
-	
+
 	@FXML
 	private TextField transactionRecc;
-	
+
 	@FXML
 	private ComboBox Types;
-	
+
 	@FXML
 	private VBox BarChart;
-	
-	
+
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
 		// TODO Auto-generated method stub
+		TransactionDao dao = new TransactionDao();
+		List<Transaction> transactions = dao.getAllTransactions();
 		transactionViews = new ArrayList<String>();
-		
+
 		Statistics stat = new Statistics();
-		
-		List<String> name = stat.returnTypes();
-		
+
+		List<String> name = stat.returnTypes(transactions);
+
 		Types.getItems().addAll(name);
-		
+
 		Stats_Interface si = new Stats_Interface();
-		
-		main.Statistics.BarChart bco = si.displayYearStats();
-		
-		
+
+		main.Statistics.BarChart bco = si.displayYearStats(transactions);
+
 		final CategoryAxis xAxis = new CategoryAxis();
 		final NumberAxis yAxis = new NumberAxis();
-		final BarChart<String,Number> bc = new BarChart<String,Number>(xAxis,yAxis);
-		
+		final BarChart<String, Number> bc = new BarChart<String, Number>(xAxis, yAxis);
+
 		xAxis.setLabel("Months");
 		yAxis.setLabel("Dollars");
 		bc.setTitle("Year Statistics " + bco.Year);
-		
+
 		XYChart.Series series1 = new XYChart.Series();
-        series1.setName("Average");       
-        
-        XYChart.Series series2 = new XYChart.Series();
-        series2.setName("Median");
-        
-        XYChart.Series series3 = new XYChart.Series();
-        series3.setName("Inflow");       
-        
-        XYChart.Series series4 = new XYChart.Series();
-        series4.setName("Outflow");     
-        
-        System.out.println(bco.MonthNames.size());
-        
-        for(int i=0; i<12; i++)
-        {
-        	System.out.println(bco.MonthNames.get(i) + " "  +bco.MonthAvg.get(i));
-        	
-        	 series1.getData().add(new XYChart.Data(bco.MonthNames.get(i), bco.MonthAvg.get(i)));
-        	 series2.getData().add(new XYChart.Data(bco.MonthNames.get(i), bco.MonthMed.get(i)));
-        	 series3.getData().add(new XYChart.Data(bco.MonthNames.get(i), bco.MonthIN.get(i)));
-        	 series4.getData().add(new XYChart.Data(bco.MonthNames.get(i), bco.MonthOUT.get(i)));
-        	 
-        	 System.out.println(bco.MonthOUT.get(i));
-        }
-        
+		series1.setName("Average");
+
+		XYChart.Series series2 = new XYChart.Series();
+		series2.setName("Median");
+
+		XYChart.Series series3 = new XYChart.Series();
+		series3.setName("Inflow");
+
+		XYChart.Series series4 = new XYChart.Series();
+		series4.setName("Outflow");
+
+		System.out.println(bco.MonthNames.size());
+
+		for (int i = 0; i < 12; i++) {
+			System.out.println(bco.MonthNames.get(i) + " " + bco.MonthAvg.get(i));
+
+			series1.getData().add(new XYChart.Data(bco.MonthNames.get(i), bco.MonthAvg.get(i)));
+			series2.getData().add(new XYChart.Data(bco.MonthNames.get(i), bco.MonthMed.get(i)));
+			series3.getData().add(new XYChart.Data(bco.MonthNames.get(i), bco.MonthIN.get(i)));
+			series4.getData().add(new XYChart.Data(bco.MonthNames.get(i), bco.MonthOUT.get(i)));
+
+			System.out.println(bco.MonthOUT.get(i));
+		}
+
 		bc.getData().addAll(series1, series2, series3, series4);
 		BarChart.getChildren().add(bc);
 	}
+
 	public void updateStatistics(ActionEvent event) {
-		
+		TransactionDao dao = new TransactionDao();
+		List<Transaction> transactions = dao.getAllTransactions();
+
 		Stats_Interface inter = new Stats_Interface();
-		
+
 		String Temp = "";
 		String type = transactionType.getText();
 		String recc = transactionRecc.getText();
-		String category = (String)Types.getValue();
+		String category = (String) Types.getValue();
 		int rec = 0;
-		
-		
-		
-		if(!recc.equals(""))
-		{
+
+		if (!recc.equals("")) {
 			rec = Integer.parseInt(recc);
 		}
-		
-		if(type.equals(""))
-		{
-			
-			List<String> list = inter.ExtremeMonths();
-			
-			for(int i=0; i<list.size(); i++)
-			{
-				Temp += list.get(i)+"\n";
+
+		if (type.equals("")) {
+
+			List<String> list = inter.ExtremeMonths(transactions);
+
+			for (int i = 0; i < list.size(); i++) {
+				Temp += list.get(i) + "\n";
+			}
+		} else if ((!type.equals("")) && recc.equals("")) {
+
+			List<String> list = inter.displayStats(transactions, type, category);
+
+			if (category != null) {
+				Temp += "Selection by Category " + category + "\n\n";
+			}
+
+			for (int i = 0; i < list.size(); i++) {
+				Temp += list.get(i) + "\n";
+			}
+		} else if ((!type.equals("")) && (!recc.equals(""))) {
+			List<String> list = inter.displayRecurring(transactions, type, rec, category);
+
+			if (category != null) {
+				Temp += "Selection by Category " + category + "\n\n";
+			}
+
+			for (int i = 0; i < list.size(); i++) {
+				Temp += list.get(i) + "\n";
 			}
 		}
-		else if((!type.equals("")) && recc.equals(""))
-		{
-			
-			
-			List<String> list = inter.displayStats(type, category);
-			
-			if(category != null)
-			{
-				Temp += "Selection by Category " + category +"\n\n"; 
-			}
-			
-			for(int i=0; i<list.size(); i++)
-			{
-				Temp += list.get(i)+"\n";
-			}
-		}
-		else if((!type.equals("")) && (!recc.equals("")))
-		{
-			List<String> list = inter.displayRecurring(type, rec, category);
-			
-			if(category != null)
-			{
-				Temp += "Selection by Category " + category+"\n\n"; 
-			}
-			
-			for(int i=0; i<list.size(); i++)
-			{
-				Temp += list.get(i)+"\n";
-			}
-		}
-		
-		
+
 		transactionName.setText(Temp);
 	}
-	
-	
-	
-	
-	
+
 }
